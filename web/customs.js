2932407449a5de300062345d28c6746bb02a7de5
tlallyzi <tlallyzi183@gmail.com>
2024-11-06 10:21:31 +0700

    Update customs.js

--- web/customs.js
+++ web/customs.js
@@ -10,5 +10,5 @@ function createButton(containerId, url) {
     document.getElementById(containerId).appendChild(button);
   }
 
-  createButton("btncnt", "https://url.mview.online/p/sub.html?=VideoHD");
-  createButton("btncnt1", "https://url.mview.online/p/sub.html?=VideoHD");
+  createButton("btncnt", "https://www.profitablecpmrate.com/zv0v12vpz?key=30f372ab2c851abf52a9473f938c9ed6");
+  createButton("btncnt1", "https://www.profitablecpmrate.com/hpieypqbx?key=373d091e1136e5d4f1a21736a886f087");
